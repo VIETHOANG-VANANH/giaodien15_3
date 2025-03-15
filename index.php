cfba94595c568c8050ad48c4fa5b18e903737e19
VIETHOANG-VANANH <phamviethoang170704@gmail.com>
2025-03-15 11:02:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="header">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="assets/icons/h1.svg" alt="EduPress Logo">
+        </div>
+
+        <!-- Navigation Menu -->
+        <nav class="nav">
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="courses.php">Courses</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="#">Page ▼</a></li>
+                <li><a href="addons.php">LearnPress Add-On</a></li>
+                <li><a href="premium.php">Premium Theme</a></li>
+            </ul>
+        </nav>
+
+        <!-- Login & Search -->
+        <div class="menu-right">
+            <a href="login.php" class="login-btn">Login / Register</a>
+            <button class="search-btn">🔍</button>
+        </div>
+    </header>
+    <!-- Banner -->
+    <section class="hero-banner">
+        <img src="assets/images/HERO%20BANNER.png" alt="Hình Banner">
+
+        <section class="categories">
+            <h2>Explore our Popular Categories</h2>
+            <div class="category-grid">
+                <div class="category-item active">
+                    <img src="assets/icons/icon.svg" alt="Art & Design">
+                    <h3>Art & Design</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/art.svg" alt="Development">
+                    <h3>Development</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/communication.svg" alt="Communication">
+                    <h3>Development</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/video.svg" alt="Videography">
+                    <h3>Development</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/photo.svg" alt="Photography">
+                    <h3>Development</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/marketing.svg" alt="Marketing">
+                    <h3>Development</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/data.svg" alt="Content Writing">
+                    <h3>Content Writing</h3>
+                    <p>38 Courses</p>
+                </div>
+                <div class="category-item active">
+                    <img src="assets/icons/business.svg" alt="Finance">
+                    <h3>Finance</h3>
+                    <p>38 Courses</p>
+                </div>
+            </div>
+        </section>
+
+
+
+
+</body>
+
+</html>
